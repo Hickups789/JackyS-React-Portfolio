e23fab5956695c7e770d8e26007ef0931eafcacf
Hickups789 <jackysantiago358@yahoo.com>
2022-10-27 15:52:23 -0400

    links

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,9 +5,9 @@ function About() {
 
   return (
     <section className="my-5" >
-      <h1 id="about"></h1>
+      <h1 id="about">About</h1>
       <div className="my-2">
-      <img src="../src/assets/images/IMG_6716.jpg" alt="jacky"/>
+      <img src="https://github.com/Hickups789/Portfolio/blob/main/images/IMG_6716.jpeg?raw=true" alt="jacky"/>
       <p>I am from Waterbury,Connecticut and been in Florida for around 18yrs.</p>
           <p> My kids are my life! Im gym obsessed and have seen a huge transformation.</p>
           <p>I love to just listen to music and drive for hours.</p>
